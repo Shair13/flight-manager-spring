a496d0045aefe0d6e2b0d590bc600f477f854199
Szer13 <shad350@gmail.com>
2024-04-18 20:44:51 +0200

    changed exceptions messages from string to json

--- src/main/java/com/example/flightmanager/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/example/flightmanager/controller/ExceptionControllerAdvice.java
@@ -6,6 +6,7 @@ import com.example.flightmanager.exception.NoAvailableSeatsException;
 import com.example.flightmanager.exception.PassengerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,32 +18,40 @@ import java.util.Map;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(FlightNotFoundException.class)
-    ResponseEntity<String> handleFlightNotFound(FlightNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    ResponseEntity<Map<String, String>> handleFlightNotFound(FlightNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getMessage(e.getMessage()));
     }
 
     @ExceptionHandler(PassengerNotFoundException.class)
-    ResponseEntity<String> handlePassengerNotFound(PassengerNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    ResponseEntity<Map<String, String>> handlePassengerNotFound(PassengerNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getMessage(e.getMessage()));
     }
 
     @ExceptionHandler(NoAvailableSeatsException.class)
-    ResponseEntity<String> handleNoAvailableSeats(NoAvailableSeatsException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    ResponseEntity<Map<String, String>> handleNoAvailableSeats(NoAvailableSeatsException e) {
+        return ResponseEntity.badRequest().body(getMessage(e.getMessage()));
     }
 
     @ExceptionHandler(DuplicatePassengerException.class)
-    ResponseEntity<String> handleDuplicatePassenger(DuplicatePassengerException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    ResponseEntity<Map<String, String>> handleDuplicatePassenger(DuplicatePassengerException e) {
+        return ResponseEntity.badRequest().body(getMessage(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<Map<Integer, String>> handleValidation(MethodArgumentNotValidException e){
-        Map<Integer, String> errors = new HashMap<>();
+    ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getAllErrors().forEach(error -> {
             String message = error.getDefaultMessage();
-            errors.put(errors.size(), message);
+            FieldError fieldError = (FieldError) error;
+            String fieldName = fieldError.getField();
+            errors.put(fieldName, message);
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    Map<String, String> getMessage(String errorMessage) {
+        Map<String, String> message = new HashMap<>();
+        message.put("error:", errorMessage);
+        return message;
     }
 }
