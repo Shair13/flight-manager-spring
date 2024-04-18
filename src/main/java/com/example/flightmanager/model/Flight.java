653c7358088ef5ef17230ffcfee4a177c5ad7dd2
Szer13 <shad350@gmail.com>
2024-04-18 20:25:38 +0200

    deleted setters from model classes

--- src/main/java/com/example/flightmanager/model/Flight.java
+++ src/main/java/com/example/flightmanager/model/Flight.java
@@ -10,13 +10,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
