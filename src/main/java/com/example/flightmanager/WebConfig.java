cb867373476a3195c8a7a8f4acb82ef0456e20a8
Szer13 <shad350@gmail.com>
2024-06-28 12:22:05 +0200

    added container react app http

--- src/main/java/com/example/flightmanager/WebConfig.java
+++ src/main/java/com/example/flightmanager/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Adres aplikacji React
+                        .allowedOrigins("http://localhost:3000", "http://react:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
